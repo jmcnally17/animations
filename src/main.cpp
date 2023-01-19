8437bb5405d106599b2f836d51dba299d1124813
jmcnally17 <joe.t.mcnally@hotmail.com>
2023-01-19 23:39:16 +0000

    Open graphics window and draw a rectangle

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+  sf::RenderWindow window(sf::VideoMode(1000, 1000), "Rectangle");
+  sf::RectangleShape rectangle(sf::Vector2f(100, 500));
+  rectangle.setFillColor(sf::Color::Green);
+  rectangle.setPosition(50, 75);
+
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+
+    window.clear();
+    window.draw(rectangle);
+    window.display();
+  }
+
+  return 0;
+}
