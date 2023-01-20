2cf35e4ebac62d0848f3916b51cb498dec5d5a6e
jmcnally17 <joe.t.mcnally@hotmail.com>
2023-01-20 18:54:32 +0000

    Draw a circle

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,14 @@
 int main()
 {
   sf::RenderWindow window(sf::VideoMode(1536, 1344), "Rectangle");
+
   sf::RectangleShape rectangle(sf::Vector2f(100, 500));
   rectangle.setFillColor(sf::Color::Green);
   rectangle.setPosition(50, 75);
+
+  sf::CircleShape circle(100, 40);
+  circle.setFillColor(sf::Color::Red);
+  circle.setPosition(50, 1100);
 
   std::cout << rectangle.getPosition().x << ", " << rectangle.getPosition().y << "\n";
 
@@ -24,6 +29,7 @@ int main()
 
     window.clear();
     window.draw(rectangle);
+    window.draw(circle);
     window.display();
     rectangle.move(0.5, 0);
     check_for_thousand(rectangle);
