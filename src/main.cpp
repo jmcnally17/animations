c75299b0e1a765faca7f969f274869b27794303c
jmcnally17 <joe.t.mcnally@hotmail.com>
2023-01-20 20:22:03 +0000

    Add escape key closing

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ int main()
     sf::Event event;
     while (window.pollEvent(event))
     {
-      if (event.type == sf::Event::Closed)
+      if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
       {
         window.close();
       }
