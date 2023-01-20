59006c25f63073d1b9e484404cb82271961414d2
jmcnally17 <joe.t.mcnally@hotmail.com>
2023-01-20 19:26:59 +0000

    Rearrange functions

--- src/check_for_thousand.cpp
+++
@@ -1,8 +0,0 @@
-#include <SFML/Graphics.hpp>
-#include <iostream>
-
-void check_for_thousand(sf::RectangleShape rectangle)
-{
-  if (rectangle.getPosition().x == 1000)
-    std::cout << "You just hit 1000!\n";
-}
--- src/main.cpp
+++ src/main.cpp
@@ -47,3 +47,9 @@ int main()
 
   return 0;
 }
+
+void check_for_thousand(sf::RectangleShape rectangle)
+{
+  if (rectangle.getPosition().x == 1000)
+    std::cout << "You just hit 1000!\n";
+}
