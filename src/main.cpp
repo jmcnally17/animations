8b4bacfdf93249c0c5f12fb832b61571b4942ca5
jmcnally17 <joe.t.mcnally@hotmail.com>
2023-01-20 18:34:59 +0000

    Add conditional to print when position hits a certain point

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
 int main()
@@ -7,19 +8,25 @@ int main()
   rectangle.setFillColor(sf::Color::Green);
   rectangle.setPosition(50, 75);
 
+  std::cout << rectangle.getPosition().x << ", " << rectangle.getPosition().y << "\n";
+
   while (window.isOpen())
   {
     sf::Event event;
     while (window.pollEvent(event))
     {
       if (event.type == sf::Event::Closed)
+      {
         window.close();
+      }
     }
 
     window.clear();
     window.draw(rectangle);
     window.display();
     rectangle.move(0.5, 0);
+    if (rectangle.getPosition().x == 1000)
+      std::cout << "You just hit 1000!\n";
   }
 
   return 0;
