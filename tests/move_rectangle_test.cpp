fc520d3ffad14af74bd22773eaf299eb0f2b177e
jmcnally17 <joe.t.mcnally@hotmail.com>
2023-01-23 17:06:01 +0000

    Remove unnecessary namespace

--- tests/move_rectangle_test.cpp
+++ tests/move_rectangle_test.cpp
@@ -1,15 +1,12 @@
 #include <gtest/gtest.h>
 #include "../include/functions.h"
 
-namespace
+TEST(MoveRectangle, AddsQuarterToXPosition)
 {
-  TEST(MoveRectangle, AddsQuarterToXPosition)
-  {
-    sf::RectangleShape rectangle(sf::Vector2f(10, 10));
-    rectangle.setPosition(50, 50);
-    moveRectangle(rectangle);
+  sf::RectangleShape rectangle(sf::Vector2f(10, 10));
+  rectangle.setPosition(50, 50);
+  moveRectangle(rectangle);
 
-    double XPosition = rectangle.getPosition().x;
-    EXPECT_EQ(XPosition, 50.25);
-  }
+  double XPosition = rectangle.getPosition().x;
+  EXPECT_EQ(XPosition, 50.25);
 }
