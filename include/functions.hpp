d1050c5332a23529258e2074f12c48b4e3ae91d2
jmcnally17 <joe.t.mcnally@hotmail.com>
2023-01-24 22:07:24 +0000

    Reset to single function for simplicity

--- include/functions.hpp
+++ include/functions.hpp
@@ -1,11 +1,5 @@
 #include <SFML/Graphics.hpp>
 
-void drawCrab(sf::RenderWindow &rWindow, const sf::Sprite &rCrabSprite);
-
-void drawShapes(sf::RenderWindow &rWindow, const sf::RectangleShape rectangle, const sf::CircleShape circle);
-
-void checkForFiveHundred(const sf::RectangleShape rectangle);
+void drawRectangle(sf::RenderWindow &rWindow, const sf::RectangleShape &rRectangle);
 
 void moveRectangle(sf::RectangleShape &rRectangle);
-
-void moveCircle(sf::CircleShape &rCircle);
--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,6 @@ int main()
   sf::RenderWindow window(sf::VideoMode(1536, 1344), "Rectangle");
 
   sf::RectangleShape rectangle(sf::Vector2f(100, 500));
-  rectangle.setFillColor(sf::Color::Green);
-  rectangle.setPosition(50, 75);
-
-  sf::CircleShape circle(100, 40);
-  circle.setFillColor(sf::Color::Red);
-  circle.setPosition(50, 1100);
-
-  sf::Texture crabTexture;
-  if (!crabTexture.loadFromFile("images/crab.png"))
-  {
-    std::cout << "Couldn't load crab image!\n";
-  }
-  sf::Sprite crabSprite;
-  crabSprite.setPosition(1000, 700);
-  crabSprite.setTexture(crabTexture);
 
   while (window.isOpen())
   {
@@ -34,13 +19,10 @@ int main()
     }
 
     window.clear();
-    drawCrab(window, crabSprite);
-    drawShapes(window, rectangle, circle);
+    drawRectangle(window, rectangle);
     window.display();
 
-    checkForFiveHundred(rectangle);
     moveRectangle(rectangle);
-    moveCircle(circle);
   }
 
   return 0;
