d01d21f40d7d65638ea0cbcf5c02efc260201acb
jmcnally17 <joe.t.mcnally@hotmail.com>
2023-01-25 00:33:24 +0000

    Make wrapper class

---
+++ include/models/rectangleShapeWrapper.hpp
@@ -0,0 +1,9 @@
+#include <SFML/Graphics.hpp>
+#include "iRectangleShape.hpp"
+
+class RectangleShapeWrapper : public IRectangleShape, public sf::RectangleShape
+{
+public:
+  RectangleShapeWrapper(sf::RectangleShape rectangle) : sf::RectangleShape::RectangleShape(rectangle) {}
+  void move(float x, float y) override { sf::RectangleShape::move(x, y); }
+};
